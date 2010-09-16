8bec555859e968a00dede0e9e23314da09297458
Patrick Weibel <p.weibel@eweibel.net>
2010-09-16 22:10:47 +0200

    NSubstitute test added

--- MockingTest/NSubstituteTest.cs
+++ MockingTest/NSubstituteTest.cs
@@ -1,9 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Mocking;
+
+using NSubstitute;
+
 namespace MockingTest
 {
 	[TestClass]
 	public class NSubstituteTest
 	{
+		[TestMethod]
+		public void TestPersist()
+		{
+			// Arrange
+			var gateway = Substitute.For<IUserGateway>();
+			var validator = Substitute.For<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			validator.Validate(user).Returns(true);
+			gateway.Persist(user).Returns(true);
+
+			// Act
+			bool bRet = user.Persist(validator);
+
+			// Assert
+			Assert.AreEqual(true, bRet);
+			validator.Received().Validate(user);
+			gateway.Received().Persist(user);
+		}
 	}
 }
