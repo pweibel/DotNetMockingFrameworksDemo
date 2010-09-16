ea2a6497d1dfa585109843e16549ea9bbf5ec0bc
Patrick Weibel <p.weibel@eweibel.net>
2010-09-16 22:19:04 +0200

    NSubstitute more tests added

--- MockingTest/NSubstituteTest.cs
+++ MockingTest/NSubstituteTest.cs
@@ -3,6 +3,7 @@
 using Mocking;
 
 using NSubstitute;
+using NSubstitute.Exceptions;
 
 namespace MockingTest
 {
@@ -15,6 +16,67 @@ namespace MockingTest
 			// Arrange
 			var gateway = Substitute.For<IUserGateway>();
 			var validator = Substitute.For<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			validator.Validate(user).Returns(true);
+			gateway.Persist(user).Returns(true);
+
+			// Act
+			bool bRet = user.Persist(validator);
+
+			// Assert
+			Assert.AreEqual(true, bRet);
+			validator.Received().Validate(user);
+			gateway.Received().Persist(user);
+		}
+
+		[TestMethod]
+		public void TestPersist_Not_Validated()
+		{
+			// Arrange
+			var gateway = Substitute.For<IUserGateway>();
+			var validator = Substitute.For<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			validator.Validate(user).Returns(false);
+			gateway.Persist(user).Returns(true);
+
+			// Act
+			bool bRet = user.Persist(validator);
+
+			// Assert
+			Assert.AreEqual(false, bRet);
+			validator.Received().Validate(user);
+			gateway.DidNotReceive().Persist(user);
+		}
+
+		[TestMethod]
+		public void TestPersist_GatewayClass()
+		{
+			// Arrange
+			var gateway = Substitute.For<UserGateway>();
+			var validator = Substitute.For<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			validator.Validate(user).Returns(true);
+			gateway.Persist(user).Returns(true);
+
+			// Act
+			bool bRet = user.Persist(validator);
+
+			// Assert
+			Assert.AreEqual(true, bRet);
+			validator.Received().Validate(user);
+			gateway.Received().Persist(user);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(SubstituteException))]
+		public void TestPersist_ValidatorClass()
+		{
+			// Arrange
+			var gateway = Substitute.For<UserGateway>();
+			var validator = Substitute.For<UserValidator>();
 			User user = new User();
 			user.Gateway = gateway;
 			validator.Validate(user).Returns(true);
