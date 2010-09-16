e49d6d52f13bd9bffec4a48e824d481bee2464d6
Patrick Weibel <p.weibel@eweibel.net>
2010-09-16 22:29:45 +0200

    FakeItEasy tests added

---
+++ MockingTest/FakeItEasyTest.cs
@@ -0,0 +1,90 @@
+﻿using FakeItEasy;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Mocking;
+
+namespace MockingTest
+{
+	[TestClass]
+	public class FakeItEasyTest
+	{
+		[TestMethod]
+		public void TestPersist()
+		{
+			// Arrange
+			var gateway = A.Fake<IUserGateway>();
+			var validator = A.Fake<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			A.CallTo(() => validator.Validate(user)).Returns(true);
+			A.CallTo(() => gateway.Persist(user)).Returns(true);
+
+			// Act
+			Assert.AreEqual(true, user.Persist(validator));
+
+			// Assert
+			A.CallTo(() => validator.Validate(user)).MustHaveHappened(Repeated.Once);
+			A.CallTo(() => gateway.Persist(user)).MustHaveHappened(Repeated.Once);
+		}
+
+		[TestMethod]
+		public void TestPersist_Not_Validated()
+		{
+			// Arrange
+			var gateway = A.Fake<IUserGateway>();
+			var validator = A.Fake<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			A.CallTo(() => validator.Validate(user)).Returns(false);
+			A.CallTo(() => gateway.Persist(user)).Returns(true);
+
+			// Act
+			bool bRet = user.Persist(validator);
+
+			// Assert
+			Assert.AreEqual(false, bRet);
+			A.CallTo(() => validator.Validate(user)).MustHaveHappened(Repeated.Once);
+			A.CallTo(() => gateway.Persist(user)).MustNotHaveHappened();
+		}
+
+		[TestMethod]
+		public void TestPersist_GatewayClass()
+		{
+			// Arrange
+			var gateway = A.Fake<UserGateway>();
+			var validator = A.Fake<IUserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			A.CallTo(() => validator.Validate(user)).Returns(true);
+			A.CallTo(() => gateway.Persist(user)).Returns(true);
+
+			// Act
+			Assert.AreEqual(true, user.Persist(validator));
+
+			// Assert
+			A.CallTo(() => validator.Validate(user)).MustHaveHappened(Repeated.Once);
+			A.CallTo(() => gateway.Persist(user)).MustHaveHappened(Repeated.Once);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ExpectationException))]
+		public void TestPersist_ValidatorClass()
+		{
+			// Arrange
+			var gateway = A.Fake<UserGateway>();
+			var validator = A.Fake<UserValidator>();
+			User user = new User();
+			user.Gateway = gateway;
+			A.CallTo(() => validator.Validate(user)).Returns(true);
+			A.CallTo(() => gateway.Persist(user)).Returns(true);
+
+			// Act
+			Assert.AreEqual(true, user.Persist(validator));
+
+			// Assert
+			A.CallTo(() => validator.Validate(user)).MustHaveHappened(Repeated.Once);
+			A.CallTo(() => gateway.Persist(user)).MustHaveHappened(Repeated.Once);
+		}
+	}
+}
